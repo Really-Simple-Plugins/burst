b170bb3320bf5573bc5cb37068d50fa4640b0b44
Hessel de Jong <info@hesseldejong.com>
2021-02-16 17:38:47 +0100

    Added a stop experiment button

--- experiments/metabox-variation.php
+++ experiments/metabox-variation.php
@@ -4,8 +4,6 @@ $experiment_id = burst_get_experiment_id_for_post($post_id);
 
 if (intval($experiment_id)) { 
 	$experiment = new BURST_EXPERIMENT($experiment_id);
-	error_log('experiment in metabox');
-	error_log(print_r($experiment, true));
 	?>
 
 <?php } ?>
@@ -30,28 +28,39 @@ if (intval($experiment_id)) {
 					</div>
 				</div>
 				<!-- <p>Fill in a name and select the type of experiment. Choose which page you want to use as a variant. Then choose the weight of your experiment. When your done click on 'Save and setup variant'. This will take you to the variant page and over there you can change the variant, choose your goal and start experimenting! </p> -->
-				<?php wp_nonce_field( 'burst_start_experiment', 'burst_nonce' ); ?>
-
-				<?php	
-				if ( ! intval($experiment_id) ) { ?>
-					<input type="hidden" value="1" name="burst_create_experiment">
-				<?php } ?>
 
 				<input type="hidden" value="<?php echo $post_id ?>" name="burst_original_post_id">
+				
 				<?php
-				BURST::$field->get_fields( 'BURST_EXPERIMENT',
-								'goal' );
-				?>
+				if ($experiment->test_running) { ?>
 
+					<?php wp_nonce_field( 'burst_stop_experiment', 'burst_nonce' ); ?>
 
-				<?php
-				BURST::$field->get_fields( 'BURST_EXPERIMENT',
-								'timeline' );
-				?>
-				<div class="burst-experiment-save-button">
-					<input class="button button-primary" name="burst_start_experiment_button"
-					        type="submit" value="<?php _e( 'Start the experiment',
-							'burst' ) ?>">
-				</div>
+					<h1>Test running</h1>
+					<div class="burst-experiment-save-button">
+						<input class="button button-error" name="burst_stop_experiment_button"
+						        type="submit" value="<?php _e( 'Stop the experiment',
+								'burst' ) ?>">
+					</div>		
+
+				<?php } elseif (intval($experiment_id) && !empty($experiment->variant_id)) { ?>
+
+					<?php wp_nonce_field( 'burst_start_experiment', 'burst_nonce' ); ?>
+
+					<?php
+					BURST::$field->get_fields( 'BURST_EXPERIMENT',
+									'goal' );
+					?>
+
+					<?php
+					BURST::$field->get_fields( 'BURST_EXPERIMENT',
+									'timeline' );
+					?>
+					<div class="burst-experiment-save-button">
+						<input class="button button-primary" name="burst_start_experiment_button"
+						        type="submit" value="<?php _e( 'Start the experiment',
+								'burst' ) ?>">
+					</div>					
+				<?php } ?>
 			</div>
 	</form>
